3cf74c7500718e8bf0c06dc82add7b6fcabbc0a9
Nullnumber123 <nullnumber123@gmail.com>
2024-11-25 18:23:23 +0530

    Update config.py
    Signed-off-by: Nullnumber123 <nullnumber123@gmail.com>

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://envs.sh/KNA.jpg"
+    "START_IMG_URL", "https://envs.sh/zpe.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://envs.sh/K0W.jpg"
